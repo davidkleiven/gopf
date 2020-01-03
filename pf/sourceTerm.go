d57d4eea81f70d0784bbd7cea757a10c946dbf09
David Kleiven <davidkleiven446@gmail.com>
2020-01-03 14:45:28 +0100

    ENH: Add source term

---
+++ pf/sourceTerm.go
@@ -0,0 +1,31 @@
+package pf
+
+import (
+	"math"
+	"math/cmplx"
+)
+
+// TimeDepSource is a generic interface for functions that can be used as sources
+type TimeDepSource func(t float64) float64
+
+// Source is structure used to add source terms to an equation
+type Source struct {
+	Pos []float64
+	f   TimeDepSource
+}
+
+// NewSource returns a new source instance
+func NewSource(pos []float64, f TimeDepSource) Source {
+	return Source{Pos: pos, f: f}
+}
+
+// Eval evaluates the fourier transformed source term and places the result in data
+func (s *Source) Eval(freq Frequency, t float64, data []complex128) {
+	for i := range data {
+		k := freq(i)
+		data[i] = complex(s.f(t), 0.0) * cmplx.Exp(-complex(0.0, 2.0*math.Pi*Dot(k, s.Pos)))
+	}
+}
+
+// Sources is a type used to represent many sources
+type Sources []Source
---
+++ pf/sourceTerm_test.go
@@ -0,0 +1,28 @@
+package pf
+
+import (
+	"math"
+	"math/cmplx"
+	"testing"
+)
+
+func demo(t float64) float64 {
+	return 2.0 * t
+}
+
+func freq(i int) []float64 {
+	return []float64{float64(i)}
+}
+
+func TestSourceTerm(t *testing.T) {
+	src := NewSource([]float64{2.5}, demo)
+	cData := make([]complex128, 2)
+	src.Eval(freq, 2.0, cData)
+	a1 := cmplx.Exp(-complex(0.0, 2.0*math.Pi*2.5*0.0))
+	a2 := cmplx.Exp(-complex(0.0, 2.0*math.Pi*2.5*1.0))
+	expect := []complex128{a1 * complex(4.0, 0.0), a2 * complex(4.0, 0.0)}
+
+	if !CmplxEqualApprox(expect, cData, 1e-10) {
+		t.Errorf("Expected\n%v\nGot\n%v\n", expect, cData)
+	}
+}
