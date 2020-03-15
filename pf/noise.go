afee05c4928ece08cda61ecc4c9dd9cf4cf7d93b
David Kleiven <davidkleiven446@gmail.com>
2020-03-15 09:48:16 +0100

    ENH: Implement conservative noise

--- pf/noise.go
+++ pf/noise.go
@@ -1,6 +1,7 @@
 package pf
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 )
@@ -17,4 +18,78 @@ type WhiteNoise struct {
 func (w *WhiteNoise) Generate(i int, bricks map[string]Brick) complex128 {
 	std := math.Sqrt(2.0 * w.Strength)
 	return complex(rand.NormFloat64()*std, 0.0)
+}
+
+// ConservativeNoise adds noise in a such a way that the field it is added to is conserved
+type ConservativeNoise struct {
+	UniquePrefix uint32
+	Strength     float64
+	Dim          int
+}
+
+// NewConservativeNoise returns an instance of ConservativeNoise with a
+// correctly initialized UniquePrefix which is used to identify derived fields
+// associated with the conservative noise
+func NewConservativeNoise(strength float64, dim int) ConservativeNoise {
+	return ConservativeNoise{
+		UniquePrefix: rand.Uint32(),
+		Strength:     strength,
+		Dim:          dim,
+	}
+}
+
+// GetCurrentName returns the name of the current field
+func (cn *ConservativeNoise) GetCurrentName(comp int) string {
+	return fmt.Sprintf("%d_current_%d", cn.UniquePrefix, comp)
+}
+
+// CurrentFieldsAreRegistered checks that all the current fields are
+// available among the bricks
+func (cn *ConservativeNoise) CurrentFieldsAreRegistered(bricks map[string]Brick) bool {
+	for comp := 0; comp < cn.Dim; comp++ {
+		if _, ok := bricks[cn.GetCurrentName(comp)]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+// Construct builds the right hand side term
+func (cn *ConservativeNoise) Construct(bricks map[string]Brick) Term {
+	if !cn.CurrentFieldsAreRegistered(bricks) {
+		panic("ConservativeCurrent: Current fields are not register. Make sure that you have registered the fields returned by RequiredDerivedFields.")
+	}
+	return func(freq Frequency, t float64, field []complex128) {
+		Clear(field)
+		for comp := 0; comp < cn.Dim; comp++ {
+			brick := bricks[cn.GetCurrentName(comp)]
+			for i := range field {
+				f := 2.0 * math.Pi * freq(i)[comp]
+				field[i] += complex(0.0, 2.0*math.Sin(f)) * brick.Get(i)
+			}
+		}
+	}
+}
+
+// OnStepFinished does nothing
+func (cn *ConservativeNoise) OnStepFinished(t float64, bricks map[string]Brick) {}
+
+// RequiredDerivedFields returns the a set of derived fields that is nessecary to register
+// in order to use conservative noise. The number of nodes in the grid is specified via
+// the numNodes argument
+func (cn *ConservativeNoise) RequiredDerivedFields(numNodes int) []DerivedField {
+	dfields := make([]DerivedField, cn.Dim)
+	for i := 0; i < cn.Dim; i++ {
+		dfields[i] = DerivedField{
+			Data: make([]complex128, numNodes),
+			Name: cn.GetCurrentName(i),
+			Calc: func(data []complex128) {
+				std := math.Sqrt(2.0 * cn.Strength)
+				for i := range data {
+					data[i] = complex(rand.NormFloat64()*std, 0.0)
+				}
+			},
+		}
+	}
+	return dfields
 }
--- pf/noise_test.go
+++ pf/noise_test.go
@@ -46,3 +46,49 @@ func TestVariance(t *testing.T) {
 		t.Errorf("Unexpected standard deviation. Expected 2.0 got %f", std)
 	}
 }
+
+func TestConservativeNoise(t *testing.T) {
+	model := NewModel()
+	N := 16
+	field := NewField("myfield", N*N, nil)
+	model.AddField(field)
+
+	noise := NewConservativeNoise(1.0, 2)
+	dfields := noise.RequiredDerivedFields(N * N)
+
+	model.RegisterUserDefinedTerm("CONSERVATIVE_NOISE", &noise, dfields)
+	model.AddEquation("dmyfield/dt = CONSERVATIVE_NOISE")
+
+	// Initialize a solver
+	solver := NewSolver(&model, []int{N, N}, 0.1)
+	solver.Solve(10, 100)
+
+	// Check that the field is real
+	tol := 1e-10
+	for i := range field.Data {
+		im := imag(field.Data[i])
+		if math.Abs(im) > tol {
+			t.Errorf("Imaginary field! Imag: %f\n", im)
+		}
+	}
+
+	// Check that the total sum is zero
+	integratedField := 0.0
+	numLargerThanStd := 0
+	for i := range field.Data {
+		integratedField += real(field.Data[i])
+
+		if math.Abs(real(field.Data[i])) > math.Sqrt(2.0) {
+			numLargerThanStd++
+		}
+	}
+
+	if math.Abs(integratedField) > tol {
+		t.Errorf("Expected that the integrated field is zero. Got %f\n", integratedField)
+	}
+
+	if numLargerThanStd == 0 {
+		t.Errorf("There are no nodes that are larger than the standard deviaton")
+	}
+
+}
