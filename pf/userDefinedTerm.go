1258e389ca5b06365cd9456b3efd690ed25b059b
David Kleiven <davidkleiven446@gmail.com>
2020-05-23 10:45:54 +0200

    ENH: Validate implicit terms on initialization

--- pf/userDefinedTerm.go
+++ pf/userDefinedTerm.go
@@ -1,5 +1,7 @@
 package pf
 
+import "github.com/davidkleiven/gopf/pfutil"
+
 // PureTerm is an interface that can be used to add
 // special terms to a PDE that cannot easily be defined via a string
 // representation. This term type is either linear or non-linear.
@@ -60,3 +62,48 @@ type MixedTerm interface {
 // GenericFunction is a generic function that may depend
 // on any of the fields
 type GenericFunction func(i int, bricks map[string]Brick) complex128
+
+// isImplicit checks that a term is indeed implicit. If a term can be handled
+// as an implicit term, it cannot change when the the fields changes.
+// The reason for this is that we use a fourier domain solver. If A, B and C
+// are fields, then a real space solver would be able to handle bilinear term
+// implicitly (e.g. dA/dt = -B*C*A), however when we apply fourier transforms
+// bilinear term is not bilinear in the fourier domain
+// (e.g. FFT(A*B*C) != FFT(A*B)*FFT(C)). Thus, bilinear terms cannot be handled
+// implicitly in the fourier domain. Thus, if a term should be handled implicitly
+// in GOPF it has to be a linear operator (constant, nabla etc.) that is independent
+// of any of the fields applied to the field. This, function tries to vary all
+// fields and confirms that the value returned by the passed term is independent
+// of all fields
+func isImplicit(t PureTerm, bricks map[string]Brick, N int, freq Frequency) bool {
+
+	// Construct a set of fields that changes the fields
+	tmpBricks := make(map[string]Brick)
+	for k, brick := range bricks {
+		tmpBricks[k] = &perturbedBrick{
+			name:         k,
+			parentBrick:  brick,
+			perturbation: complex(0.2, 0.0),
+		}
+	}
+	func1 := t.Construct(bricks)
+	func2 := t.Construct(tmpBricks)
+	array1 := make([]complex128, N)
+	array2 := make([]complex128, N)
+
+	func1(freq, 0.0, array1)
+	func2(freq, 0.0, array2)
+	return pfutil.CmplxEqualApprox(array1, array2, 1e-10)
+}
+
+// perturbedBrick is a helper structure used to test implicit terms when
+// the underlying fields are perturbed
+type perturbedBrick struct {
+	name         string
+	parentBrick  Brick
+	perturbation complex128
+}
+
+func (pb *perturbedBrick) Get(i int) complex128 {
+	return pb.parentBrick.Get(i) + pb.perturbation
+}
