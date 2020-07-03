769988887acd27b921e31a1e1e573c900a09c1db
David Kleiven <davidkleiven446@gmail.com>
2020-07-03 13:43:50 +0200

    ENH: Add CSV file IO

--- pf/fileIO.go
+++ pf/fileIO.go
@@ -2,8 +2,12 @@ package pf
 
 import (
 	"encoding/binary"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"strconv"
 
 	"github.com/davidkleiven/gopf/pfutil"
 )
@@ -88,4 +92,92 @@ func SaveFloat64(fname string, data []float64) {
 	}
 	defer out.Close()
 	binary.Write(out, binary.BigEndian, data)
+}
+
+// CsvIO writes data to text csv text files
+type CsvIO struct {
+	Prefix     string
+	DomainSize []int
+}
+
+// SaveFields stores the results in CSV files. The format
+// X, Y, Z, field1, field2, field3
+// etc.
+func (cio *CsvIO) SaveFields(s *Solver, epoch int) {
+	if cio.DomainSize == nil {
+		panic("Domain size not given. Data will not be written to file\n")
+	}
+
+	if pfutil.ProdInt(cio.DomainSize) != s.Model.NumNodes() {
+		msg := fmt.Sprintf("Inconsistent domain size. Expected %d nodes, got %d\n", s.Model.NumNodes(), pfutil.ProdInt(cio.DomainSize))
+		panic(msg)
+	}
+
+	header := []string{"X", "Y", "Z"}
+	for _, f := range s.Model.Fields {
+		header = append(header, f.Name)
+	}
+
+	fname := cio.Prefix + fmt.Sprintf("_%d.csv", epoch)
+	out, err := os.Create(fname)
+	if err != nil {
+		log.Fatalf("Could not open file: %s\n", err)
+		return
+	}
+	defer out.Close()
+
+	writer := csv.NewWriter(out)
+	defer writer.Flush()
+
+	writer.Write(header)
+	record := make([]string, len(header))
+	pos := make([]int, 3)
+	for i := 0; i < s.Model.NumNodes(); i++ {
+		position := pfutil.Pos(cio.DomainSize, i)
+		copy(pos, position)
+		for j := 0; j < 3; j++ {
+			record[j] = fmt.Sprintf("%d", pos[j])
+		}
+		for j, f := range s.Model.Fields {
+			record[j+3] = fmt.Sprintf("%f", real(f.Data[i]))
+		}
+		writer.Write(record)
+	}
+}
+
+// LoadCSV loads data from CSV file and returns an array of fields
+func LoadCSV(fname string) []Field {
+	infile, err := os.Open(fname)
+	if err != nil {
+		msg := fmt.Sprintf("Could not open file: %s\n", err)
+		panic(msg)
+	}
+	defer infile.Close()
+
+	reader := csv.NewReader(infile)
+	header, err := reader.Read()
+	if err != nil {
+		log.Fatalf("Could not read header: %s\n", header)
+		return nil
+	}
+
+	fields := make([]Field, len(header)-3)
+	for i := range fields {
+		fields[i].Name = header[i+3]
+		fields[i].Data = []complex128{}
+	}
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			return fields
+		} else if err != nil {
+			log.Fatalf("Could not read record: %s\n", err)
+		}
+
+		for i := 0; i < len(fields); i++ {
+			v, _ := strconv.ParseFloat(record[i+3], 64)
+			fields[i].Data = append(fields[i].Data, complex(v, 0.0))
+		}
+	}
 }
--- pf/fileIO_test.go
+++ pf/fileIO_test.go
@@ -2,7 +2,10 @@ package pf
 
 import (
 	"math"
+	"os"
 	"testing"
+
+	"github.com/davidkleiven/gopf/pfutil"
 )
 
 func TestSaveLoad(t *testing.T) {
@@ -28,4 +31,36 @@ func TestSaveLoad(t *testing.T) {
 			t.Errorf("Expected %f got %f\n", data[i], re)
 		}
 	}
+}
+
+func TestSaveLoadCsv(t *testing.T) {
+	model := NewModel()
+	N := 8
+	f1 := NewField("field1", N*N, nil)
+	f2 := NewField("field2", N*N, nil)
+	for i := 0; i < N*N; i++ {
+		f1.Data[i] = complex(float64(i-i*i), 0.0)
+		f2.Data[i] = complex(float64(i), 0.0)
+	}
+	model.AddField(f1)
+	model.AddField(f2)
+
+	solver := NewSolver(&model, []int{N, N}, 0.1)
+
+	csvIO := CsvIO{
+		Prefix:     "my_csv_file",
+		DomainSize: []int{N, N},
+	}
+
+	csvIO.SaveFields(solver, 0)
+
+	loaded := LoadCSV("my_csv_file_0.csv")
+	tol := 1e-8
+	fields := []Field{f1, f2}
+	for i := range loaded {
+		if !pfutil.CmplxEqualApprox(loaded[i].Data, fields[i].Data, tol) {
+			t.Errorf("Expected\n%v\nGot\n%v\n", fields[i].Data, loaded[i].Data)
+		}
+	}
+	os.Remove("my_csv_file_0.csv")
 }
