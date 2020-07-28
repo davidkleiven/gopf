ed54358b73d85b80b49f1d90dbd4bede9d5a2dbe
davidkleiven <davidkleiven446@gmail.com>
2020-07-28 12:45:25 +0200

    ENH: Add box and ball shape (#125)

---
+++ pfutil/shapes_test.go
@@ -0,0 +1,46 @@
+package pfutil
+
+import (
+	"testing"
+
+	"gonum.org/v1/gonum/floats"
+)
+
+func TestShapes(t *testing.T) {
+	for i, test := range []struct {
+		shape Shape
+		grid  Grid
+		want  []float64
+	}{
+		{
+			shape: &Box{Diagonal: []float64{5.1, 5.1}},
+			grid:  NewGrid([]int{8, 8}),
+			want: []float64{1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+				1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+				1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+				0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+				1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0},
+		},
+		{
+			shape: &Ball{Radius: 2.1},
+			grid:  NewGrid([]int{8, 8}),
+			want: []float64{1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+				1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+				1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0},
+		},
+	} {
+		Draw(test.shape, &test.grid, nil, 1.0)
+
+		if !floats.EqualApprox(test.want, test.grid.Data, 1e-10) {
+			t.Errorf("Test #%d: Expected\n%v\nGot\n%v\n", i, test.want, test.grid.Data)
+		}
+	}
+}
